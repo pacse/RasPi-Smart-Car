8173aa05b2efe22f7dba86a2b8684741ad693f80
pacse <68709871+pacse@users.noreply.github.com>
2025-10-18 17:25:46 +1300

    Update buzzer.py
    Uses RPi.GPIO :)
    Simplified comments
    Added docstrings galore
    replaced set_state with on/off

--- Code/Server/buzzer.py
+++ Code/Server/buzzer.py
@@ -1,28 +1,42 @@
-import time
-from gpiozero import OutputDevice
+from .__init__ import GPIO
 
 class Buzzer:
-    def __init__(self):
+    """
+    Simple class to control a buzzer
+    """
+    def __init__(self) -> None:
         """Initialize the Buzzer class."""
-        self.PIN = 17                            # Set the GPIO pin for the buzzer
-        self.buzzer_pin = OutputDevice(self.PIN) # Initialize the buzzer pin
+        self.PIN = 17                   # Set buzzer pin #
+        GPIO.setup(self.PIN, GPIO.OUT)  # Configure buzzer pin as output
 
-    def set_state(self, state: bool) -> None:
-        """Set the state of the buzzer."""
-        self.buzzer_pin.on() if state else self.buzzer_pin.off() # Turn on or off the buzzer based on the state
+    def on(self) -> None:
+        """Turn on the buzzer."""
+        GPIO.output(self.PIN, True)
+
+    def off(self) -> None:
+        """Turn off the buzzer."""
+        GPIO.output(self.PIN, False)
+
 
     def close(self) -> None:
-        """Close the buzzer pin."""
-        self.buzzer_pin.close()           # Close the buzzer pin to release the GPIO resource
+        """Cleanup the buzzer pin."""
+        GPIO.cleanup(self.PIN)
+
 
 if __name__ == '__main__':
-    print('Program is starting ... ')     # Print a message indicating the start of the program
-    buzzer = Buzzer()                     # Create an instance of the Buzzer class
+    """
+    Test the Buzzer class:
+    turn on and off 3 times
+    """
+    import time
+
+    print('Program is starting ... ')
+    buzzer = Buzzer()
     try:
         for _ in range(3):
-            buzzer.set_state(True)        # Turn on the buzzer
-            time.sleep(0.1)               # Wait for 0.1 second
-            buzzer.set_state(False)       # Turn off the buzzer
-            time.sleep(0.1)               # Wait for 0.1 second
+            buzzer.on()
+            time.sleep(0.1)
+            buzzer.off()
+            time.sleep(0.1)
     finally:
-        buzzer.close()                    # Ensure the buzzer pin is closed when the program is interrupted
+        buzzer.close()
