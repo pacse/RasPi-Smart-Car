bbab66539c209f9e792dd3828adf0f63569cb59f
pacse <68709871+pacse@users.noreply.github.com>
2025-10-19 15:01:11 +1300

    Create controller.py
    What will merge with the input class (and likely be renamed) to control the car from a controller
    
    Funcs
      - controll movement with 1 joystick
      - strafe left/right

---
+++ Code/Server/components/motors/controller.py
@@ -0,0 +1,47 @@
+from .car import Car
+
+class Controller:
+    """
+    Controller for the car's motors.
+    """
+    def __init__(self, car: Car) -> None:
+        self.car = car
+
+
+    def from_joystick(self, y_axis: int, x_axis: int) -> None:
+        """
+        Control the car using a single joystick.
+
+        :param y_axis: Y-axis value from joystick (-1 to 1).
+        :param x_axis: X-axis value from joystick (-1 to 1).
+        """
+
+        speed = round(y_axis * 100)  # forward/backward speed
+        turn = round(x_axis * 100)   # turning adjustment
+
+        left_v = speed + turn
+        right_v = speed - turn
+
+        self.car.set_motor_speeds(FL = left_v, FR = right_v,
+                              BL = left_v, BR = right_v)
+
+
+    def strafe_left(self, speed: int) -> None:
+        """
+        Strafe the car left with mecanum wheels.
+
+        :param speed: Speed percentage (0 - 100).
+        """
+
+        self.car.set_motor_speeds(FL = -speed, FR = speed,
+                              BL = speed, BR = -speed)
+
+    def strafe_right(self, speed: int) -> None:
+        """
+        Strafe the car right with mecanum wheels.
+
+        :param speed: Speed percentage (0 - 100).
+        """
+
+        self.car.set_motor_speeds(FL = speed, FR = -speed,
+                              BL = -speed, BR = speed)
