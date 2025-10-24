0b33b0b1fdc3474524738f655c5a9a83bd2b22cf
pacse <68709871+pacse@users.noreply.github.com>
2025-10-24 13:02:50 +1300

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,7 @@ Test script for movement
 """
 
 from Server.components.motors import Car, Motor
+from time import sleep
 
 motor_pins = [
               (11, 12),  # Front-left motor pins
@@ -20,6 +21,8 @@ FL.set_speed(50)
 FR.set_speed(50)
 BL.set_speed(50)
 BR.set_speed(50)
+
+sleep(2)
 
 FL.cleanup()
 FR.cleanup()
