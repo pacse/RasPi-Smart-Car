ff95031cd3ac3c47f0777510341d222d7e301e3b
pacse <packard.seth@gmail.com>
2025-10-27 14:26:26 +1300

    added __init__ file to Code directory

---
+++ Code/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package for RasPi Smart Car project.
+
+Handles:
+- Client-side functionality
+- Server-side functionality
+- Testing
+"""
+
+from RPi import GPIO # type: ignore (not coding on Raspberry Pi)
+from .config import logging, DEFAULT
