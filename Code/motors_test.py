e260e56163680a1ad265a52bae4e46746c77b18a
pacse <68709871+pacse@users.noreply.github.com>
2025-10-29 11:50:23 +1300

    Update motors_test.py

--- Code/motors_test.py
+++ Code/motors_test.py
@@ -4,10 +4,6 @@ from time import sleep
 
 car = Car(MOTOR_PINS)
 car.set_motor_speeds(50, 50, 50, 50)
-sleep(2)
+sleep(5)
 car.stop_all_motors()
-
-car.turn_left(60)
-sleep(1)
-car.turn_right(60)
-sleep(1)
+car.cleanup()
