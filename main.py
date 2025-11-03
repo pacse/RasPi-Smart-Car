dfc43f75e8cf067fb37270fa3a4b1d68692d2331
pacse <68709871+pacse@users.noreply.github.com>
2025-11-03 13:00:30 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from Code.Server.motors import Motor, config
 from Code.Server.motors.car import Car
+import time
 
 car = Car(config.MOTOR_PINS)
 car.set_motor_speeds(50, 50, 50, 50)
 
+time.sleep(5)
 
+car.set_motor_speeds(0, 0, 0, 0)
+
+car.cleanup()
