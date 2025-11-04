bbfa4d6ad105b2ecee58b782d393633da897566a
pacse <68709871+pacse@users.noreply.github.com>
2025-11-04 13:11:39 +1300

    Update controller_test.py
    Updated imports
    deleted unused code

--- controller_test.py
+++ controller_test.py
@@ -1,43 +1,19 @@
-from Code.Code.controller import Car_Controller
-from Code.Code.motors import Car, Controller
-from Code.Code.motors.config import MOTOR_PINS
+from Code.car_controller import Car_Controller
+from Code import Car, Controller
+from Code.config import MOTOR_PINS
 
-import time
-
+# set up pygame for joystick handling
 import pygame
 
 pygame.init()
 pygame.joystick.init()
 
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
 car = Car(MOTOR_PINS)
 controller = Controller(car)
 
-# for motor in car.motors:
-#     motor.set_speed(50)
-#     time.sleep(2)
-#     motor.set_speed(0)
-# car.cleanup()
-
-"""try:
-    while True:
-        car.forward(20)
-finally:
-    controller.cleanup()
-    print("Cleanup done.")"""
-
-
-# controller.from_joystick(1, 0)
-# time.sleep(2)
-# controller.from_joystick(0, 1)
-# time.sleep(2)
-# controller.strafe_left(50)
-# time.sleep(2)
-# controller.strafe_right(50)
-# time.sleep(2)
-# controller.cleanup()
-
 car_controller = Car_Controller()
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
 print(joysticks)
 
@@ -54,7 +30,7 @@ try:
         if not car_controller.strafe:
             controller.from_joystick(-car_controller.pwm_y, -car_controller.pwm_x)
         else:
-            controller.strafe_from_joystick(car_controller.trigger_l, car_controller.trigger_r)
+            controller.strafe_from_joystick(car_controller.trig_L, car_controller.Trig_R)
         #time.sleep(0.25)
 finally:
     controller.cleanup()
