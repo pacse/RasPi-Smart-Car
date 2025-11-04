94bd5080dc64aa7629fdae778130026c99306f17
pacse <68709871+pacse@users.noreply.github.com>
2025-11-04 13:09:18 +1300

    Update Code/__init__.py

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,11 +1,7 @@
 """
 Package for RasPi Smart Car project.
-
-Handles:
-- Client-side functionality
-- Server-side functionality
-- Testing
 """
 
 from RPi import GPIO # type: ignore (not coding on Raspberry Pi)
-from .config import logging, MOTOR_PINS
+
+from .motors import Motor, Car, Controller
