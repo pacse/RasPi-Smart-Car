592b270ab9a394f0c12dc15cfe3fcfe45596707a
pacse <packard.seth@gmail.com>
2025-11-06 21:21:41 +1300

    Updated Code __init__

--- Code/__init__.py
+++ Code/__init__.py
@@ -5,4 +5,4 @@ Package for RasPi Smart Car project.
 from RPi import GPIO # type: ignore (not coding on Raspberry Pi)
 GPIO.setmode(GPIO.BOARD)
 
-from .motors import Motor, Car, Controller
+from .motors import Car, Controller
