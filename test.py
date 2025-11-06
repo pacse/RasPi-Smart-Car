7e7cc1f4acb8616ebc817addc98e98135581c5c3
pacse <68709871+pacse@users.noreply.github.com>
2025-11-06 15:46:07 +1300

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,27 @@
 Run tests from test suite
 """
 
-from Code.tests import test_Controller, test_Motor, test_Car
+#from Code.tests import test_Controller, test_Motor, test_Car
 
 def run_all_tests():
     """
     Run all tests.
     """
-    test_Motor()
-    test_Car()
-    test_Controller()
-    print("All tests pass?")
+    #test_Motor()
+    #test_Car()
+    #test_Controller()
+    #print("All tests pass?")
+
+from Code.motors import Motor
+
+L = Motor((15, 16))
+R = Motor((11, 12), 1000)
+
+L.set_speed(100)
+R.set_speed(100)
+import time
+time.sleep(5)
+L.set_speed(0)
+R.set_speed(0)
+L.cleanup()
+R.cleanup()
